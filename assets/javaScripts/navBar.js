f721d3e79ba9298c7d76198d4a968acae1f023d3
Stephane Horne <stephane.horne@gmail.com>
2020-03-23 12:26:53 -0400

    Added dropdown/reset functionality for hobbies

--- assets/javaScripts/navBar.js
+++ assets/javaScripts/navBar.js
@@ -3,9 +3,22 @@ const HOBBIESOPTIONS = document.querySelectorAll(".hobbies");
 const TRIVIAOPTIONS = document.querySelectorAll(".trivia");
 
 function dropdown(menuOption){
-    console.log(menuOption.id);
+    if (menuOption.id == "hobbies"){
+        for (i = 0; i < HOBBIESOPTIONS.length; i++){
+            HOBBIESOPTIONS[i].style = "display: inline-block;";
+        }
+    }
+}
+
+function reset(menuOption){
+    if (menuOption.id == "hobbies"){
+        for (i = 0; i < HOBBIESOPTIONS.length; i++){
+            HOBBIESOPTIONS[i].style = "display: hidden";
+        }
+    }
 }
 
 for (i = 0; i < NAVMAIN.length; i++){
     NAVMAIN[i].addEventListener("mouseenter", function(){dropdown(this);}, false);
+    NAVMAIN[i].addEventListener("mouseleave", function(){reset(this);}, false);
 }
